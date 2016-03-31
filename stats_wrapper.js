3e21ef8553f47bf8b8eb4f20f3b8d831582f74d7
Jonathan Casarrubias <c_jcasarrubias@groupon.com>
2016-03-31 12:50:02 -0600

    Added stats wrapper functionallity

--- stats_wrapper.js
+++ stats_wrapper.js
@@ -1,3 +1,5 @@
+'use strict';
+var async = require('async');
 /**
  *  Wrapper JS Mixin
  *  @Author Jonathan Casarrubias
@@ -11,20 +13,49 @@
 module.exports = function(Model, ctx) {
     // Create dynamic statistic method
     Model[ctx.method] = function() {
-        ctx.params = new ParamsBuilder(arguments).build();
-        ctx.items.forEach();
+        ctx.result = {};
+        ctx.args   = arguments;
+        ctx.next   = ctx.args[arguments.length - 1];
+        async.each(ctx.wraps, (item, next) => {
+            ctx.args[ctx.args.length - 1] = (err, dataset) => {
+                if (err) return next(err);
+                ctx.result[item] = dataset;
+                next();
+            };
+            Model[item].apply(Model, Array.from(ctx.args));
+        }, err => ctx.next(err, ctx.result));
     };
     /**
      * Dynamically Register Endpoint
      */
     Model.remoteMethod(ctx.method, {
-        http: { path: ctx.endpoint, verb: ctx.http.method || 'get' },
-        accepts: ctx.http.accepts,
-        returns: ctx.http.returns,
+        http: { path: ctx.endpoint, verb: 'get' },
+        accepts: new AcceptBuilder(ctx).build(),
+        returns: { type: 'object', root: true },
         description: ctx.description
     });
 };
 /**
- * Params Builder
+ * Builds Parameters object for dynamic remote method
  */
-class 
+class AcceptBuilder {
+    /**
+     * Setters
+     */
+    constructor(ctx) { this.ctx = ctx; }
+    /**
+     * Parse params according ctx type
+     */
+    build() {
+        let accepts = [];
+        if (this.ctx.type === "relation" || this.ctx.type === "nested")
+            accepts.push({ arg: 'id', type: 'string', required: true, description: 'Model id' });
+        if (this.ctx.type === "relation" && !this.ctx.relation)
+            accepts.push({ arg: 'relation', type: 'string', required: true, description: 'Relationship name' });
+        if (this.ctx.type === "nested")
+            accepts.push({ arg: 'nested', type: 'string', required: true, description: 'Nested array property name' });
+        accepts.push({ arg: 'range', type: 'string', required: true, description: 'Scale range (daily, weekly, monthly, annual)' });
+        accepts.push({ arg: 'where', type: 'object', description: 'Statement to filter list of items to be processed' });
+        return accepts;
+    }
+}
