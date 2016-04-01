24fc1cb557d2eacafec6d5124f88b3c00eeb984a
victorhqc <victorhqc@gmail.com>
2016-04-01 16:48:47 -0600

    Clean pk builder Instead of using manual logic, used method getIdName()

--- builders/pk-builder.js
+++ builders/pk-builder.js
@@ -5,10 +5,6 @@
 module.exports = class PrimaryKeyBuilder {
     constructor(Model) { this.Model = Model; }
     build() {
-        let pk = 'id';
-        if (!this.Model.settings.idInjection)
-        for (let key in this.Model.rawProperties)
-        if (this.Model.rawProperties[key].id) pk = key;
-        return pk;
+        return  this.Model.getIdName();
     }
 }
